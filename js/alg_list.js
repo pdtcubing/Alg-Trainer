d429a5aec34095628327f8933764bce1628b9016
pdtcubing <65262710+pdtcubing@users.noreply.github.com>
2020-12-30 23:22:51 +1300

    Update alg_list.js

--- js/alg_list.js
+++ js/alg_list.js
@@ -1,6 +1,6 @@
 var tcll_plus = {
 	"Hammer": ["R' F R F' R' F R F'", "F' U2 F U' R U' R'", "(U) R U R' F R F' R U R' U R'", "(y' U or T-slot in BR) R' U' R U' R2 F R F' R", "(U) R' U R U' R U' R2 F R2 F'", "(U2) R' F' R U2 R U R' F2"]
-}
+};
 
 var eg_full = {
     "S": ["(U') F' L U2 F2 R U' x'", "R U R' F2 U F R U R' ", "(U') R' F R U2 R U' R2 F2 R F'","F' U R U' R' U F R U R'", "(U) R U' R' U R U' R' U F R U' R'", "R' F R2 U' R' U L F' L' F"],
